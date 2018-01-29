ec04cbb117b0747fc27e1622a024adf33515a18e
승현 <rbfksep@naver.com>
2018-01-29 17:11:20 +0900

    승현 1/29

--- spring_webproject/src/main/java/com/bc/web_project/mypageController/MypageController.java
+++ spring_webproject/src/main/java/com/bc/web_project/mypageController/MypageController.java
@@ -9,4 +9,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MypageController {
 	@RequestMapping(value="01cl.do", method=RequestMethod.GET)
 	public String main() {return "LSH/01cl";}
+	@RequestMapping(value="02pl.do", method=RequestMethod.GET)
+	public String pl() {return "LSH/02pl";}
+	@RequestMapping(value="03re.do", method=RequestMethod.GET)
+	public String re() {return "LSH/03re";}
+	@RequestMapping(value="04tip.do", method=RequestMethod.GET)
+	public String tip() {return "LSH/04tip";}
+	@RequestMapping(value="05qna.do", method=RequestMethod.GET)
+	public String qna() {return "LSH/05qna";}
+	@RequestMapping(value="set.do", method=RequestMethod.GET)
+	public String set() {return "LSH/set";}
 }
